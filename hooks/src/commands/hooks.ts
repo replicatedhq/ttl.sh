95a73da1338d2a142c8a39442a795dc6ea037afa
Marc Campbell <marc.e.campbell@gmail.com>
2019-05-16 00:24:48 +0000

    Remove more dependencies

--- hooks/src/commands/hooks.ts
+++ hooks/src/commands/hooks.ts
@@ -1,7 +1,6 @@
 import * as util from "util";
 import { Server } from "../server/server";
 import { logger } from "../logger";
-import * as Sentry from "@sentry/node";
 import { param } from "../util";
 
 exports.name = "hooks";
@@ -21,11 +20,6 @@ async function main(argv): Promise<any> {
   process.on('SIGTERM', function onSigterm () {
     logger.info(`Got SIGTERM, cleaning up`);
     process.exit();
-  });
-
-  Sentry.init({
-    dsn: param.get("SENTRY_DSN_API"),
-    environment: param.get("ENVIRONMENT"),
   });
 
   await new Server().start();
