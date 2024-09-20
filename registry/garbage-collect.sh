be168f863b7da9ed69ca729e7dc5b73055e12583
Ethan Mosbaugh <ethan@replicated.com>
2024-09-20 13:54:00 -0700

    f

--- registry/garbage-collect.sh
+++ registry/garbage-collect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  sleep 24h
+  sleep 72h # every 3 days
   echo "Starting garbage collection..."
   registry garbage-collect /etc/docker/registry/config.yml || true
 done
