08d1ed1a486d2bf7cd49b4846423500e662d2b9a
Ethan Mosbaugh <emosbaugh@gmail.com>
2024-10-08 15:38:34 -0500

    Revert "feat: run blob reaper every 72h (#152)"
    This reverts commit b7ab57fa9be3ff1452206516e3a3efb8ad587dda.

--- registry/garbage-collect.sh
+++ registry/garbage-collect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  sleep 72h # every 3 days
+  sleep 1m
   echo "Starting garbage collection..."
   registry garbage-collect /etc/docker/registry/config.yml || true
 done
